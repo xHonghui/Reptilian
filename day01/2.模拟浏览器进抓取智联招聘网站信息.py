15e59cffc9488c9ff797514ba6cfedf1af294950
xuhonghui <xuhonghui@baogongyoucai.com>
2018-06-27 17:57:46 +0800

    网络爬虫第一天

--- day01/2.模拟浏览器进抓取智联招聘网站信息.py
+++ day01/2.模拟浏览器进抓取智联招聘网站信息.py
@@ -7,13 +7,16 @@ import selenium.webdriver  # 模拟浏览器
 
 def get_web_data(url):
     driver = selenium.webdriver.Firefox()  # 调用火狐浏览器
-    driver.get(url)   # 设置 url
+    driver.get(url)  # 设置 url
     web_data = driver.page_source  # 获取源代码数据
     restr = "<em>(\\d+)</em>"
     regex = re.compile(restr, re.IGNORECASE)
     target_list = regex.findall(web_data)
     driver.close()
-    return target_list[0]
+    if len(target_list) > 0:
+        return target_list[0]
+    else:
+        return None
 
 
 def get_data(keyword, address):
@@ -22,7 +25,8 @@ def get_data(keyword, address):
             url = "https://sou.zhaopin.com/jobs/searchresult.ashx?jl=" + bytes(address[i]) + "&kw=" + keyword[
                 j] + "&sm=0&p=1"
             target_num = get_web_data(url)
-            print address[i], keyword[j], target_num
+            if target_num is not None:  # 非空判断
+                print address[i], keyword[j], target_num
 
 
 keyword = ["java", "android", "python", "h5"]
